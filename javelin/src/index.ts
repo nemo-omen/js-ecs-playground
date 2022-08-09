4f7ee44586eac00aa69cb0e147bf1825fc2db64f
nemo-omen <nemo.omen@gmail.com>
2022-08-09 11:24:48 -0500

    Flail around

--- javelin/src/index.ts
+++ javelin/src/index.ts
@@ -1,7 +1,7 @@
-import { createWorld, component, string, number, arrayOf, boolean } from '@javelin/ecs';
+import { createWorld, createQuery, component, string, number, arrayOf, boolean } from '@javelin/ecs';
 import type { World } from '@javelin/ecs';
+import { createHrtimeLoop } from "@javelin/hrtime-loop";
 
-const world: World = createWorld();
 
 const Health = {
   hitPoints: number,
@@ -29,12 +29,51 @@ const Character = {
   description: string,
 };
 
-const health = component(Health);
-const bag = component(Bag);
-const inventory = component(Inventory);
+const BattleState = {
+  inBattle: boolean
+};
+
+const Hit = {
+  isHit: boolean,
+  damage: number
+};
+
+const world: World = createWorld();
+
 const alive = component(Alive);
-const conscious = component(Conscious);
+const bag = component(Bag);
+const battleState = component(BattleState);
 const characterComponent = component(Character);
+const conscious = component(Conscious);
+const health = component(Health);
+const hit = component(Hit);
+const inventory = component(Inventory);
 
-const character = world.create(characterComponent, health, bag, inventory, alive, conscious);
+const chars = createQuery(Character, Alive, Conscious);
 
+const character = world.create(characterComponent, hit, health, bag, inventory, alive, conscious, battleState);
+
+const healthSystem = (world: World) => chars((character, [battleState, hit, health, alive, conscious]) => {
+
+  if (battleState.inBattle === true) {
+    if (hit.isHit) {
+      health.hitPoints -= hit.damage;
+    }
+  }
+
+  if (health.hitPoints < 10) {
+    conscious.awake = false;
+  }
+
+  if (health.hitPoints <= 0) {
+    alive.isLiving = false;
+  }
+});
+
+world.addSystem(healthSystem);
+
+function tick(dt: number) {
+  world.tick(dt);
+}
+
+const loop = createHrtimeLoop(1000, clock => tick(clock.dt));
